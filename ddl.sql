f1ffb3287020431f2d731099cd53c295e38efe42
Andre Duarte <andre.duarte@rezolve.com>
2019-06-17 01:41:14 -0300

    Feature: adding initial ddl file

--- ddl.sql
+++ ddl.sql
@@ -1,35 +1,28 @@
-create table user_purchase
+CREATE TABLE user_purchase
 (
-    id                              serial      not null
-        constraint user_purchase_pkey
-            primary key,
-    cpf                             varchar(20) not null
-        constraint user_purchase_cpf_key
-            unique,
-    private                         boolean,
-    incompleto                      boolean,
-    data_ultima_compra              date,
-    ticket_medio                    double precision,
-    ticket_ultima_compra            double precision,
-    loja_mais_frequente             varchar(20),
-    loja_ultima_compra              varchar(20),
-    cpf_valido                      boolean,
-    cnpj_loja_mais_frequente_valido boolean,
-    cnpj_loja_ultima_compra_valido  boolean
+    id SERIAL PRIMARY KEY,
+    cpf VARCHAR(20) NOT NULL UNIQUE,
+    private                         BOOLEAN,
+    incompleto                      BOOLEAN,
+    data_ultima_compra              DATE,
+    ticket_medio                    DOUBLE PRECISION,
+    ticket_ultima_compra            DOUBLE PRECISION,
+    loja_mais_frequente             VARCHAR(20),
+    loja_ultima_compra              VARCHAR(20),
+    cpf_valido                      BOOLEAN,
+    cnpj_loja_mais_frequente_valido BOOLEAN,
+    cnpj_loja_ultima_compra_valido  BOOLEAN
 );
-
-alter table user_purchase
-    owner to neoway;
-
 
 
 CREATE TYPE queue_worker_status AS ENUM ('success', 'fail');
 
-
-
-CREATE TABLE logs (
-    id VARCHAR(36) UNIQUE PRIMARY KEY,
+CREATE TABLE status (
+    id SERIAL PRIMARY KEY ,
+    message_id VARCHAR(36)  ,
     completion_time TIMESTAMP,
     status queue_worker_status,
-    message jsonb
-)
+    message jsonb,
+    part INT,
+    total_parts INT
+);
